665b7f3aea2bf76ba3acea8156fcf4fbe990ad56
Dusa-Saiteja <157966003+Dusa-Saiteja@users.noreply.github.com>
2024-07-09 12:41:20 +0530

    Add files via upload

---
+++ dor.js
@@ -0,0 +1,40 @@
+let join=document.getElementById("btn1");
+join.addEventListener("click",()=>{
+    window.open("./join.htm")
+})
+
+// let div=document.getElementById("signin");
+// let display=0;
+
+// let btn=document.getElementById("btn2");
+// btn.addEventListener("click",()=>{
+//     if(display==0){
+//         div.style.display="block";
+//         display=1;
+//     }
+//     else{
+//         div.style.display="none";
+//         display=0;
+//     }
+// })
+
+// let div1=document.getElementById("signin").style.display="none";
+// let div2=document.getElementById("signin");
+// let x=document.getElementById("x");
+// x.addEventListener("click",()=>{
+//     div2.style.display="none";
+// })
+
+
+let signin=document.getElementById("btn2");
+signin.onclick=function () {
+    window.open("./Signinpage.html")
+}
+
+// let open=document.getElementById("x");
+// open.onclick=function display() {
+//     window.open("./main.htm")
+// }
+
+
+
